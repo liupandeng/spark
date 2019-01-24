7ae140f88c08a1c59f08e37d208b0c42602c6c12
liupandeng <345433326@qq.com>
2019-01-24 15:47:22 +0800

    streamingwordcount nc -lk 8888

--- src/main/scala/cn/edu360/day9/StreamingWordCount.scala
+++ src/main/scala/cn/edu360/day9/StreamingWordCount.scala
@@ -7,13 +7,13 @@ import org.apache.spark.{SparkConf, SparkContext}
 /**
   * Created by zx on 2017/10/17.
   */
-object SteamingWordCount {
+object StreamingWordCount {
 
   def main(args: Array[String]): Unit = {
 
     //离线任务是创建SparkContext，现在要实现实时计算，用StreamingContext
 
-    val conf = new SparkConf().setAppName("SteamingWordCount").setMaster("local[2]")
+    val conf = new SparkConf().setAppName("StreamingWordCount").setMaster("local[2]")
     val sc = new SparkContext(conf)
     //StreamingContext是对SparkContext的包装，包了一层就增加了实时的功能
     //第二个参数是小批次产生的时间间隔
@@ -23,7 +23,7 @@ object SteamingWordCount {
     //从一个socket端口中读取数据
     //在Linux上用yum安装nc
     //yum install -y nc
-    val lines: ReceiverInputDStream[String] = ssc.socketTextStream("192.168.1.207", 8888)
+    val lines: ReceiverInputDStream[String] = ssc.socketTextStream("192.168.11.212", 8888)
     //对DSteam进行操作，你操作这个抽象（代理，描述），就像操作一个本地的集合一样
     //切分压平
     val words: DStream[String] = lines.flatMap(_.split(" "))
